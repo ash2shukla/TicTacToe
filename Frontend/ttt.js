ef658dc96c22058eee4855bf87e37fc750bb3139
Ashish Shukla <ashish.shukla@hitachiconsulting.com>
2018-10-11 00:46:28 +0530

    SVG animations support related comment

--- Frontend/ttt.js
+++ Frontend/ttt.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
     var sema = undefined;
 
     // svgs for X and O
+    // SVG animations are disabled in windows on battery saver mode so X might not render properly at battery saver
     var x = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><line class="path line" fill="none" stroke="#D06079" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="34.4" y1="37.9" x2="95.8" y2="92.3"/><line class="path line" fill="none" stroke="#D06079" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="95.8" y1="38" x2="34.4" y2="92.2"/></svg>'
     var o = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2"><circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/></svg>'
 
